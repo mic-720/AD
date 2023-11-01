18168ea82de01bf1cb0fda4fba3d0342e771b656
mic-720 <118971635+mic-720@users.noreply.github.com>
2023-11-01 23:49:58 +0530

    Add files via upload

---
+++ SubarrayReverseSorted1.java
@@ -0,0 +1,39 @@
+import java.util.Arrays;
+
+public class SubarrayReverseSorted1 {
+    public static void reverse(int[] arr,int i,int j){
+        while(i<j){
+            int temp=arr[i];
+            arr[i]=arr[j];
+            arr[j]=temp;
+            i++;
+            j--;
+        }
+    }
+    public static boolean sortArr(int[] arr){
+        int x=-1;
+        int y=-1;
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                if(x==-1){
+                    x=i;
+                }
+                y=i+1;
+            }
+        }
+        if(x!=-1){
+            reverse(arr, x, y);
+            for(int i=0;i<arr.length-1;i++){
+                if(arr[i]>arr[i+1]){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        int[] arr={1,2,5,4,6,3};
+        boolean ans=sortArr(arr);
+        System.out.println(ans);
+    }    
+}
---
+++ SubarrayReverseSorted2.java
@@ -0,0 +1,35 @@
+// Java program to check whether reversing a  
+// sub array make the array sorted or not 
+public class SubarrayReverseSorted2 {
+    public static void main(String[] args){
+        int[] arr={1,2,5,4,3,6};
+        //make the copy of the given array
+        //sort the copied array for comparision
+        int[] copy={1,2,3,4,5,6};
+        int left=arr.length-1;;
+        int right=0;
+        boolean ans=true;
+        //finding the first mis-match
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=copy[i]){
+                left=i;
+                break;
+            }
+        }
+        //finding the last mis-match
+        for(int i=arr.length-1;i>=0;i--){
+            if(arr[i]!=copy[i]){
+                right=i;
+                break;
+            }
+        }
+        //checking if the sub array is decreasing or not
+        for(int i=left;i<=right-1;i++){
+            if(arr[i]<arr[i+1]){
+                ans=false;
+                break;
+            }
+        }
+        System.out.println(ans);
+    }
+}
